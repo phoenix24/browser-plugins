00218100ef9f7485030f39c370dbad658e36601f
gopi.daiict <gopi.daiict@6eecbd12-de5c-11de-82ea-1f113a1b7651>
2009-12-09 13:27:45 +0000

    moving the binary extensions folder

--- notes/main.js
+++ notes/main.js
@@ -3,18 +3,26 @@
  */
 var sync_count = 0;
 var syncTimeout = 1000 * 2; // 1 second.
+var notes;
+var current_note;
 
-function main() {
-	var notes = localStorage["notes"];
+function init() {
+	notes = localStorage["notes"];
+	
+	//currently comma seperated notes
+	notes = notes.split(",");
+	current_note = notes[0];
+	notes = notes.splice(1, notes.length);
+	
 	var notesDIV = document.getElementById("notes_area");
-	notesDIV.value = notes;
+	notesDIV.value = localStorage[current_note];
 }
 
 function save_notes() {
 	sync_count += 1;
 	var notesDIV = document.getElementById("notes_area");
 	var notes = notesDIV.value;
-	localStorage["notes"] = notes;
+	localStorage[current_note] = notes;
 
 	var syncStatusDIV = document.getElementById("savedbtn");
 	syncStatusDIV.innerHTML = "save : " + sync_count;
@@ -43,6 +51,8 @@ function settings_notes() {
 
     var container = document.getElementById("container");
     container.style.display = "none";
+    
+    showNotes();
 }
 
 window.addEventListener("load", function(e) {
@@ -52,3 +62,37 @@ window.addEventListener("load", function(e) {
 	var backbtn = document.getElementById("backbtn");
 	backbtn.addEventListener("click", container_notes, false);
 }, false);
+
+function createnote() {
+	var newnote = document.getElementById("notename");
+	newnote = newnote.value;
+	
+	//append the new note to the global notes list.
+	notes.push(newnote);
+	localStorage["notes"] = notes.join(",");
+	localStorage[newnote] = "";
+}
+
+function showNotes() {
+	var notescontainer = document.getElementById("notescontainer");
+	
+	for (i=0; i<notes.length;i++) {
+		var noteDiv = document.createElement("div");
+		noteDiv.className = "note";
+		
+		var noteCheckbox = document.createElement("input");
+		noteCheckbox.type = "checkbox";
+		noteCheckbox.name = notes[i];
+		noteCheckbox.id = notes[i];
+		noteCheckbox.className = "note";
+		
+		var noteLabel = document.createElement("label");
+//		noteLabel.for = notes[i];
+		noteLabel.innerHTML = "  " + notes[i];
+		
+		noteDiv.appendChild(noteCheckbox);
+		noteDiv.appendChild(noteLabel);
+		
+		notescontainer.appendChild(noteDiv);
+	}
+}
