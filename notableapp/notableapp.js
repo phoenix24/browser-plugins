3e802f5160d247feaec20cba375a6b94f4e759ab
gopi.daiict <gopi.daiict@6eecbd12-de5c-11de-82ea-1f113a1b7651>
2009-12-15 11:24:17 +0000

    adding the auto-increment to notable app table

--- notableapp/notableapp.js
+++ notableapp/notableapp.js
@@ -1,28 +1,13 @@
 /**
  * @author chaitanya
  */
-var db;
-try {
-    db = openDatabase("NotableAppDB", "1.0", "NotableApp Captures Table", 20000);
-
-    db.transaction (function(tx) {
-        tx.executeSql ("SELECT id FROM NotableApp", [], 
-            function(tx, result) { 
-                console.log("count is " + result.rows.length);
-                return result.rows.length; 
-            },
-            function(tx, error) {
-                tx.executeSql("CREATE TABLE NotableApp (id REAL UNIQUE, title TEXT, url TEXT, image BLOB)", [], function(r) {
-                    console.log("table successfully created. ");
-            });
-        });
-    });
-} catch (err) {
-    console.log("failed to open the db.");
-}
 
 function Capture() {
-    var self = this;
+	var self 	= this;
+    this.db 	= notableapp.init();
+    
+    console.log("db : " + this.db);
+    
     this.view = function () {
         // enforcing delay. SQLite is slow.
         window.setTimeout(function(){}, 300);
@@ -41,13 +26,13 @@ function Capture() {
         });
     };
     this.log = function() {
-        console.log("object log: " + this +", "+ self.title +", "+ self.url +", " + this.image);
+        console.log("object log: "+ self.title +", "+ self.url +", " + this.image);
     };
     this.save = function() {
         var cap = this;
-        console.log("object save: " + cap.id +", "+ cap.title +", "+ cap.url +", " + cap.image);
-        db.transaction(function(tx){
-            tx.executeSql("INSERT INTO NotableApp (id, title, url, image) VALUES (?, ?, ?, ?)", [cap.id, cap.title, cap.url, cap.image]);
+//        console.log("object save: " + cap.title +", "+ cap.url +", " + cap.image);
+        self.db.transaction(function(tx){
+            tx.executeSql("INSERT INTO NotableApp (title, url, image) VALUES (?, ?, ?)", [cap.title, cap.url, cap.image]);
         });
     };
     this.remove = function() {
@@ -56,7 +41,7 @@ function Capture() {
     this.display = function () {
 
         var tmp, scrshot = document.getElementsByClassName("screenshot")[0];
-        db.transaction(function(tx){
+        self.db.transaction(function(tx){
             tx.executeSql("SELECT id, title, url, image FROM NotableApp", [], function(tx, result){
                 for (var i = 0; i < result.rows.length; ++i) {
                     var row = result.rows.item(i);
@@ -65,7 +50,7 @@ function Capture() {
                     cap.url = row['url'];
                     cap.title = row['title'];
                     cap.image = row['image'];
-                    console.log("object saved: " + cap.id +", "+ cap.title +", "+ cap.url +", " + cap.image);
+                    console.log("object saved: "+ cap.title +", "+ cap.url +", " + cap.image);
 
                     tmp = scrshot.cloneNode(true);
                     tmp.className = "visible";
@@ -139,10 +124,8 @@ function captureNew () {
     chrome.tabs.captureVisibleTab(null, function(img) {
         cap.image = img;
     });
-    cap.id    = 1;
-    console.log("id of the capture is : " + cap.id);
     
-//  enforcing a delay. SQLite seems to be sloooow!
+    // enforcing a delay. SQLite seems to be sloooow!
     window.setTimeout(function(){
         cap.display();
         cap.save();
@@ -153,8 +136,25 @@ function captureNew () {
 var notableapp = (function () {
 	return {
 		init : function () {
-			console.log("app's initialized!");
-			
+			var result;
+			try {
+			    db = openDatabase("NotableAppDB", "1.0", "NotableApp Captures Table", 20000);
+			    db.transaction (function(tx) {
+			        tx.executeSql ("SELECT id FROM NotableApp", [], function(tx, result) { 
+			            console.log("records count is " + result.rows.length);
+			        },
+			        function(tx, error) {
+			            tx.executeSql("CREATE TABLE IF NOT EXISTS NotableApp (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, title TEXT, url TEXT, image BLOB)", [], 
+			    			function(r) {
+			    				console.log("table successfully created. ");
+			    			});
+			    		}
+			       );
+			    });
+			} catch (err) {
+			    console.log("failed to open the db.");
+			}
+			return db;
 		},
 		totalCaptures : function () {
 			console.log("fetching the total number of captures.");
